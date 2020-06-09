82b7db8df43a8465b77e5d02ce4505dc9c425309
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-09 23:28:40 +0700

    :zap: [category-controller]: Implemented edit and update functions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -62,7 +62,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::find($id);
+        return view('category.edit',compact('category'));
     }
 
     /**
@@ -74,7 +75,10 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $category = Category::find($id);
+        $category->name = $request->get('name');
+        $category->save();
+        return redirect()->route('category.index')->with('message','Category Updated');
     }
 
     /**
