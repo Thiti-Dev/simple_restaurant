798cd6029b45e1ed643480bb17eb1f937d1b3ce0
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-09 23:46:43 +0700

    :art: [index-blade]: Created modal for delete with confirmation txt

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -26,7 +26,7 @@
                             @if(count($categories) > 0 )
                                 @foreach ($categories as $key=>$category)
                                 <tr>
-                                    <th scope="row">{{ $key }}</th>
+                                    <th scope="row">{{ $key+1 }}</th>
                                     <td>{{ $category->name }}</td>
                                     <td>
                                         <a href="{{ route('category.edit',$category->id) }}">
@@ -34,12 +34,37 @@
                                         </a>
                                     </td>
                                     <td>
-                                        <a href="">
-                                            <form action="{{ route('category.destroy',$category->id) }}" method="POST">@csrf
-                                                {{ method_field('DELETE') }}
-                                                <button class="btn btn-outline-danger">Delete</button>
-                                            </form>
-                                        </a>
+
+                                        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModal{{ $category->id }}">
+                                            Delete
+                                        </button>
+
+                                        <div class="modal fade" id="exampleModal{{ $category->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <form action="{{ route('category.destroy',$category->id) }}" method="POST">@csrf
+                                                    {{ method_field('DELETE') }}
+
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Are you sure that you want to delete {{ $category->name }} category
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                        <button type="submit" class="btn btn-outline-danger">
+                                                            Delete
+                                                        </button>
+                                                        </div>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                          </div>
                                     </td>
                                 </tr>
                                 @endforeach
